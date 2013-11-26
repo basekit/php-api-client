1289ec79dc8a5e3187c789c105281cbd0c2fb962
Simon Best <simon@basekit.com>
2013-11-26 17:33:55 +0000

    Initial port of API client from main code base to separate repo.

---
+++ src/BaseKit/Api/Client.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BaseKit\Api;
+
+use Guzzle\Common\Collection;
+use Guzzle\Plugin\Oauth\OauthPlugin;
+use Guzzle\Service\Client;
+use Guzzle\Service\Description\ServiceDescription;
+
+class Client extends Client
+{
+    public static function factory($config = array())
+    {
+        $default = array();
+
+        $required = array(
+            'base_url',
+            'consumer_key',
+            'consumer_secret',
+            'token',
+            'token_secret',
+        );
+
+        $config = Collection::fromConfig($config, $default, $required);
+
+        $client = new self($config->get('base_url'), $config);
+        $client->addSubscriber(new OauthPlugin($config->toArray()));
+
+        $client->setDescription(
+            ServiceDescription::factory(
+                __DIR__ . '/../../../basekit.json'
+            )
+        );
+
+        // @todo Add cache plugin
+
+        return $client;
+    }
+}
